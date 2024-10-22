8dc398f67f31e4c21bfa825d76b2093b2508dfc1
MaxChand1er <maxthomaschandler@gmail.com>
2024-10-22 09:44:03 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 sheetId = "1_BlmEIzj9PHMkKceLILv7pM2YOgHLWMdL8RnDwN5o0c"
 const sheetLink = 'https://docs.google.com/spreadsheets/d/'+sheetId+'/gviz/tq?tqx=out:json&tq&gid=0'
-fetch(sheetLink)
-    .then(response => response.text())
-    .then(data => document.querySelector("#output").textContent = data)
-    .catch(error => {
-        // Handle errors
-        console.error('Error:', error);
-    });
 
+function fetchSheetData() {
+    return fetch(sheetLink)
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector("#output").textContent = data;
+            return data; // Return the data for further use
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            throw error; // Rethrow to handle it in the calling function
+        });
+}
+
+// Run the fetch operation
+fetchSheetData();
+    
